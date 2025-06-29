82c1c09de52ade9736f6c368b3de119819811580
darr2208 <65313535+darr2208@users.noreply.github.com>
2025-06-28 19:39:23 -0500

    Update mapa.py

--- ProyectoCuencas/src/mapa.py
+++ ProyectoCuencas/src/mapa.py
@@ -5,42 +5,6 @@ def mostrar_mapa_dibujable(coordenadas):
     lat, lon = coordenadas
     m = folium.Map(location=coordenadas, zoom_start=13, tiles="OpenStreetMap")
 
-    folium.TileLayer(
-        tiles='https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-        attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
-        name='Stamen Terrain'
-    ).add_to(m)
-
-    folium.TileLayer(
-        tiles='https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
-        attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
-        name='Stamen Toner'
-    ).add_to(m)
-
-    folium.TileLayer(
-        tiles='https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg',
-        attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
-        name='Stamen Watercolor'
-    ).add_to(m)
-
-    folium.TileLayer(
-        tiles='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-        attr='© OpenStreetMap contributors © CARTO',
-        name='CartoDB Positron'
-    ).add_to(m)
-
-    folium.TileLayer(
-        tiles='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-        attr='© OpenStreetMap contributors © CARTO',
-        name='CartoDB Dark Matter'
-    ).add_to(m)
-
-    folium.TileLayer(
-        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        attr='Tiles © Esri',
-        name='Satélite (Esri)'
-    ).add_to(m)
-
     delta = 0.01
     rombo_coords = [
         [lat + delta, lon],
@@ -65,8 +29,6 @@ def mostrar_mapa_dibujable(coordenadas):
         icon=folium.Icon(color="red", icon="info-sign")
     ).add_to(m)
 
-    folium.LayerControl().add_to(m)
-
     st_folium(m, width=900, height=600)
 
     return {
@@ -76,5 +38,3 @@ def mostrar_mapa_dibujable(coordenadas):
             "coordinates": [[ [c[1], c[0]] for c in rombo_coords ]]
         }
     }
-
-
